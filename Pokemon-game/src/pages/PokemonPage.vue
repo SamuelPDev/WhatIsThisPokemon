129f970da4d85bc924e3aa97cdbca143185637ff
SamuelPDev <Samuelperalta129@gmail.com>
2022-12-27 12:20:31 -0400

    first commit

---
+++ Pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <h1>Who is this pokemon?</h1>
+    <PokemonPicturesVue />
+    <PokemonOptionsVue />
+</template>
+
+<script setup>
+import PokemonPicturesVue from '../components/PokemonPictures.vue';
+import PokemonOptionsVue from '../components/PokemonOptions.vue';
+
+
+</script>
